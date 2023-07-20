da369d99c23715252da67ce799936b91102abe05
kittisak191 <97100702+kittisak191@users.noreply.github.com>
2023-07-20 16:55:50 +0700

    first commit

--- src/component/project.tsx
+++ src/component/project.tsx
@@ -25,6 +25,20 @@ function Project() {
             <div className="row row-project">
               <div className="item col-lg-4 col-md-6 col-sm-12">
                 <div className="card project-card">
+                  <img className="img-fluid  image" src="/img/shop.png" />
+                  <div className="link">
+                    <a href="https://api-shop-omega.vercel.app/">
+                      <FontAwesomeIcon className="icon" icon={faChrome} />
+                    </a>
+                    <a href="https://github.com/kittisak191/Api-shop">
+                      <FontAwesomeIcon className="icon" icon={faGithub} />
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="item col-lg-4 col-md-6 col-sm-12">
+                <div className="card project-card">
                   <img className="img-fluid  image" src="/img/wash.jpg" />
                   <div className="link">
                     <a href="https://capable-chebakia-faa082.netlify.app/">
